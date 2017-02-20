1631b79458f6a32a3dbb79910132c69bac92d302
zaturnvalley <zacharycmason@gmail.com>
2017-02-20 13:29:33 -0800

    fixed syntax errs, running server

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var express = require('express');
 var request = require('request');
 var ejsLayouts = require('express-ejs-layouts');
 var bodyParser = require('body-parser');
+var path = require('path');
 
 
 //Global Variables
@@ -11,5 +12,12 @@ var db = require('./models');
 
 //Settings & Use Statements
 app.set('view engine', 'ejs');
-app.use(exppress.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, '/public')));
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(ejsLayouts);
+
+app.get('/', function(req,res){
+  res.send('success');
+});
+
+app.listen(3000);
